74bf8882b19e627a643161f769e8181e6e69288c
atqr1 <matt@sryden.com>
2024-04-20 10:43:38 +0100

    minor update

--- routes/instance.js
+++ routes/instance.js
@@ -4,4 +4,22 @@ const { v4: uuidv4 } = require('uuid');
 const axios = require('axios');
 const { db } = require('../handlers/db.js'); // Ensure db.js is properly set up to use Keyv
 
+router.ws("/console/:id", async (ws, req) => {
+    if (!req.user) return ws.end();
+
+    const instance = 
+});
+
+async function connectToConsole(id, node) {
+    try {
+        const ws = new WebSocket('http://' + node.address + ':' + node.port);
+        ws.onopen = () => {
+            ws.send(JSON.stringify({ "event": "auth", "args": [`${node.apiKey}`] }));
+        };
+        return ws;
+    } catch (error) {
+        return null;
+    }
+}
+
 module.exports = router;
