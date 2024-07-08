ee3595355781b0bab1efc728c90de0b4f0ce9226
Matt James <mattj@Matts-MacBook-Pro.local>
2024-07-08 17:38:00 +0100

    fix tags on edit node

--- routes/instance.js
+++ routes/instance.js
@@ -233,6 +233,62 @@ router.get("/instance/:id/files/create", async (req, res) => {
     res.render('createFile', { req, user: req.user, name: await db.get('name') || 'Skyport', logo: await db.get('logo') || false });
 });
 
+/**
+ * GET /instance/:id/files/rename/:file
+ */
+router.get("/instance/:id/files/rename/:file", async (req, res) => {
+    if (!req.user) {
+        return res.redirect('/');
+    }
+
+    const { id, file } = req.params;
+    if (!id || !file) {
+        return res.redirect('../instances');
+    }
+
+    const instance = await db.get(id + '_instance');
+    if (!instance) {
+        return res.status(404).send('Instance not found');
+    }
+
+    // Authorization check
+    const isAuthorized = await isUserAuthorizedForContainer(req.user.userId, instance.ContainerId);
+    if (!isAuthorized) {
+        return res.status(403).send('Unauthorized access to this instance.');
+    }
+
+    if (!instance || !instance.VolumeId) {
+        return res.redirect('../instances');
+    }
+
+    let query = req.query.path ? `?path=${req.query.path}` : '';
+
+    if (instance.Node && instance.Node.address && instance.Node.port) {
+        const RequestData = {
+            method: 'get',
+            url: `http://${instance.Node.address}:${instance.Node.port}/fs/${instance.VolumeId}/files/rename/${file}${query}`,
+            auth: {
+                username: 'Skyport',
+                password: instance.Node.apiKey
+            },
+            headers: { 
+                'Content-Type': 'application/json'
+            }
+        };
+
+        try {
+            const response = await axios(RequestData);
+
+            res.redirect('/instance/' + id + '/files' + query)
+        } catch (error) {
+            const errorMessage = error.response && error.response.data ? error.response.data.message : 'Connection to node failed.';
+            res.status(500).render('500', { error: errorMessage, req, user: req.user, name: await db.get('name') || 'Skyport', logo: await db.get('logo') || false });
+        }
+    } else {
+        res.status(500).send('Invalid instance node configuration');
+    }
+});
+
 router.post("/instance/:id/files/upload", upload.array('files'), async (req, res) => {
     if (!req.user) {
         return res.status(401).send('Authentication required');
