35f3e8fc99fe1fb0896754d3340fd692e8eb260f
tfcprivt <141457761+privt00@users.noreply.github.com>
2024-07-16 21:58:27 +0200

    Create bs-config.js

---
+++ bs-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    proxy: "http://localhost:3001",
+    files: ["views/**/*.ejs"],
+    port: 2000,
+    ui: {
+      port: 3002
+    },
+    open: true,
+    notify: false
+  };
+  
