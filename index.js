4cfd927047ca95bc642c3bda593e842f222eac5e
Mtq4 <164923658+mtq4@users.noreply.github.com>
2024-08-02 10:10:00 -0700

    Update index.js

--- index.js
+++ index.js
@@ -64,6 +64,12 @@ app.use(
   })
 );
 
+app.use((req, res, next) => {
+  res.locals.ogTitle = config.ogTitle;
+  res.locals.ogDescription = config.ogDescription;
+  next();
+});
+
 
 /* remove the comments for faster loading
 app.use((req, res, next) => {
