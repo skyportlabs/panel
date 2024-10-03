f6057b9fe942c9fd890c53d33dc4f61cc0661522
privt00 <noahunterweger3@gmail.com>
2024-10-03 18:51:17 +0200

    Added the option to use MySQL

--- handlers/db.js
+++ handlers/db.js
@@ -1,4 +1,30 @@
-const Keyv = require('keyv');
-const db = new Keyv('sqlite://skyport.db');
+const config = require('../config.json');
 
-module.exports = { db }
+if (!config.databaseURL) {
+    throw new Error('Database URL not set in config.json');
+}
+
+let db;
+
+if (config.databaseURL.startsWith('sqlite')) {
+    const Keyv = require('keyv');
+    db = new Keyv('sqlite://skyport.db');
+} else {
+    const Keyv = require('@keyvhq/core');
+    const KeyvMysql = require('@keyvhq/mysql');
+
+    const mysqlConfig = {
+        url: config.databaseURL,
+        table: 'skyport',
+        keySize: 255,
+    };
+
+    db = new Keyv({
+        store: new KeyvMysql(mysqlConfig.url, {
+            table: mysqlConfig.table,
+            keySize: mysqlConfig.keySize
+        })
+    });
+}
+
+module.exports = { db };
